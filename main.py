da3f3c2c510dbdb932249b3dad51628780aa6ead
Nicole Yeager <nyeager95@gmail.com>
2024-10-31 13:01:35 -0600

    added files

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def add(a, b):
+    return a + b
+
+def reverse_string(s):
+    return s[::-1]
+
+def is_even(n):
+    return n % 2 == 0
---
+++ test_main.py
@@ -0,0 +1,4 @@
+import unittest
+
+from main import add, reverse_string, is_even
+
