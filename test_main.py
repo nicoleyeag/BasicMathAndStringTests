1d3888d32d2ebf491a4a83d1d3b3968a410a22ae
Nicole Yeager <nyeager95@gmail.com>
2024-11-01 08:42:29 -0600

    add_test

--- test_main.py
+++ test_main.py
@@ -2,3 +2,9 @@ import unittest
 
 from main import add, reverse_string, is_even
 
+class TestBasicFunctions(unittest.TestCase):
+
+    def add_test(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-2, 3), 1)
+        self.assertEqual(add(-10, -5), 15)
