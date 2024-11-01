e20f0f44d442a78a5f52c31be1922214619c744a
Nicole Yeager <nyeager95@gmail.com>
2024-11-01 08:58:53 -0600

    reverse_string_test

--- test_main.py
+++ test_main.py
@@ -1,10 +1,17 @@
 import unittest
-
 from main import add, reverse_string, is_even
 
 class TestBasicFunctions(unittest.TestCase):
 
-    def add_test(self):
+    def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-2, 3), 1)
-        self.assertEqual(add(-10, -5), 15)
+        self.assertEqual(add(-10, -5), -15)
+
+    def test_revstr(self):
+        self.assertEqual(reverse_string('potato'), 'otatop')
+        self.assertEqual(reverse_string('coffee'), 'eeffoc')
+        self.assertEqual(reverse_string(''), '')
+
+if __name__ == '__main__':
+    unittest.main()
